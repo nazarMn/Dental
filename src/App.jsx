01912c1c55c8a6f900059d8a32726343a1353dbd
MarkivNazar <69341802+nazarMn@users.noreply.github.com>
2024-11-02 16:49:48 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import  Header  from './components/Header/Header'
+
+function App() {
+
+  return (
+    <div className='wrap'>
+
+    <Header />
+    
+    </div>
+  )
+}
+
+export default App
