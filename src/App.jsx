22ceef9d7bc9db1964cf12bb9cd3ad81c17cdd52
MarkivNazar <nazarmn2008@gmail.com>
2024-11-12 22:57:04 +0200

    create components Appointment Form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 // import './Responsive.css';
 import Header from './components/Header/Header';
 import EmailSend from "./components/EmailSend/EmailSend";
+import AppointmentForm from "./components/AppointmentForm/AppointmentForm";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -234,88 +235,7 @@ function App() {
           <h2>Make an Appointment</h2>
           <p>Dental or oral health is concerned with your teeth, gums and mouth. <br /> Healthy mouth free of infections, injuries, and other problems.</p>
         </div>
-        
-        <div className="appointmentBottom">
-          <div className="appointmentBottomLeft">
-            <h2>Appointment</h2>
-            <form className="appointmentForm" onSubmit={handleSubmit}>
-              <div className="appointmentFormBox">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Patient Name"
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-             
-
-              
-                <select name="gender" value={formData.gender} onChange={handleChange}>
-                  <option value="">Gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-
-              <div className="appointmentFormBox">
-                <input
-                  type="tel"
-                  name="phone"
-                  placeholder="Phone number"
-                  value={formData.phone}
-                  onChange={handleChange}/>
-              
-
-              
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className="appointmentFormBox">
-                <select name="department" value={formData.department} onChange={handleChange}>
-                  <option value="">Department</option>
-                  <option value="general">General</option>
-                  <option value="surgery">Surgery</option>
-                  <option value="pediatrics">Pediatrics</option>
-                </select>
-            
-
-             
-                <input
-                  type="date"
-                  name="date"
-                  value={formData.date}
-                  onChange={handleChange}
-                />
-              </div>
-
-          
-              <div className="appointmentFormBox">
-                <textarea
-                  name="details"
-                  placeholder="More details"
-                  value={formData.details}
-                  onChange={handleChange}
-                />
-                </div>
-       
-       <div className="appointmentFormBtm">
-        <div className="appointmentFormBtmBox">
-              <button>Send Massage</button>
-              </div>
-              </div>
-            </form>
-          </div>
-          <div className="appointmentBottomRight">
-            <img src={doctor5} alt="Doctor" />
-          </div>
-        </div>
+        <AppointmentForm/>
       </div>
 
 
---
+++ src/components/AppointmentForm/AppointmentForm.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import './AppointmentForm.css'
+
+export default function AppointmentForm() {
+   
+    return (
+        <div className="appointmentBottom">
+          <div className="appointmentBottomLeft">
+            <h2>Appointment</h2>
+            <form className="appointmentForm" onSubmit={handleSubmit}>
+              <div className="appointmentFormBox">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Patient Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+             
+
+              
+                <select name="gender" value={formData.gender} onChange={handleChange}>
+                  <option value="">Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+
+              <div className="appointmentFormBox">
+                <input
+                  type="tel"
+                  name="phone"
+                  placeholder="Phone number"
+                  value={formData.phone}
+                  onChange={handleChange}/>
+              
+
+              
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="appointmentFormBox">
+                <select name="department" value={formData.department} onChange={handleChange}>
+                  <option value="">Department</option>
+                  <option value="general">General</option>
+                  <option value="surgery">Surgery</option>
+                  <option value="pediatrics">Pediatrics</option>
+                </select>
+            
+
+             
+                <input
+                  type="date"
+                  name="date"
+                  value={formData.date}
+                  onChange={handleChange}
+                />
+              </div>
+
+          
+              <div className="appointmentFormBox">
+                <textarea
+                  name="details"
+                  placeholder="More details"
+                  value={formData.details}
+                  onChange={handleChange}
+                />
+                </div>
+       
+       <div className="appointmentFormBtm">
+        <div className="appointmentFormBtmBox">
+              <button>Send Massage</button>
+              </div>
+              </div>
+            </form>
+          </div>
+          <div className="appointmentBottomRight">
+            <img src={doctor5} alt="Doctor" />
+          </div>
+        </div>
+    )
+}
