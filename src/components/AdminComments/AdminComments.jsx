2f1b004748d189690580b7a15946cede72620556
MarkivNazar <nazarmn2008@gmail.com>
2024-11-19 23:38:47 +0200

    created styles for Admin Comments

--- src/components/AdminComments/AdminComments.jsx
+++ src/components/AdminComments/AdminComments.jsx
@@ -2,10 +2,48 @@ import React from 'react';
 import './AdminComments.css';
 
 const AdminComments = () => {
-
   return (
     <div className="adminComments">
+      <div className="adminCommentsLeft">
+        <div className="adminCommentsLeftTop">
+          <h1>Comments</h1>
+        </div>
 
+        <div className="adminCommentsLeftBottom">
+        </div>
+      </div>
+
+      <div className="horizontalLine"></div>
+
+      <div className="adminCommentsRight">
+        <div className="adminCommentsRightTop">
+          <h2>Create a comment</h2>
+        </div>
+
+        <div className="adminCommentsRightBottom">
+        <div className="createCommentBox">
+      <input
+        type="text"
+        placeholder="Name"
+        className="createCommentInput"
+      />
+      <textarea
+        type="text"
+        placeholder="Comment"
+        className="createCommentTextarea"
+      />
+      
+      <select id="rating" defaultValue="1" className="createCommentSelect">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button className="createCommentButton">Create</button>
+    </div>
+        </div>
+      </div>
     </div>
   );
 };
