0cc4624d1ccf6794d0599c355fa06d2464f45ec2
MarkivNazar <nazarmn2008@gmail.com>
2024-11-24 23:07:48 +0200

    3

---
+++ src/components/RegistrationPopup/RegistrationPopup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './RegistrationPopup.css'
+
+const RegistrationPopup = () => {
+    return (
+        <div className="registrationPopup">
+         
+        </div>
+    )
+}
+
+export default RegistrationPopup
