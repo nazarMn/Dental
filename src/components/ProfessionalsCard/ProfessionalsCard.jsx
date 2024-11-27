f0a70fa50a7eea4ddfa5db438a2fb3ccf8c02181
MarkivNazar <nazarmn2008@gmail.com>
2024-11-27 21:50:36 +0200

    import Swiper

--- src/components/ProfessionalsCard/ProfessionalsCard.jsx
+++ src/components/ProfessionalsCard/ProfessionalsCard.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import './ProfessionalsCard.css';
 import stethoscope from './../../assets/stethoscope.svg';
 
+
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
 const ProfessionalsCard = ({ imgSrc, title, specialist }) => {
     return (
         <div className="professionalsCard">
