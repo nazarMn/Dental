c999e64182a5bbe195673c82acf2cb2ef2d281cd
MarkivNazar <nazarmn2008@gmail.com>
2024-12-15 22:54:43 +0200

    RegistrationPopup

--- src/components/RegistrationPopup/RegistrationPopup.jsx
+++ src/components/RegistrationPopup/RegistrationPopup.jsx
@@ -1,21 +1,55 @@
+// import React from "react";
+// import './RegistrationPopup.css'
+
+// const RegistrationPopup = () => {
+//     return (
+//         <div className="registrationPopup">\
+//             <h1>Registration</h1>
+//             <form>
+//                 <input type="text" placeholder="Name" />
+//                 <input type="text" placeholder="Email" />
+//                 <input type="text" placeholder="Password" />
+//                 <input type="text" placeholder="Confirm Password" />
+//                 <button>Register</button>
+//             </form>
+            
+         
+//         </div>
+//     )
+// }
+
+// export default RegistrationPopup
+
+
+
+
+
 import React from "react";
-import './RegistrationPopup.css'
+import './RegistrationPopup.css';
 
 const RegistrationPopup = () => {
     return (
-        <div className="registrationPopup">\
-            <h1>Registration</h1>
-            <form>
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Email" />
-                <input type="text" placeholder="Password" />
-                <input type="text" placeholder="Confirm Password" />
-                <button>Register</button>
-            </form>
-            
-         
+        <div className="registrationPopup">
+            <div className="popupContainer">
+                <h1>Register</h1>
+                <form>
+                    <div className="inputGroup">
+                        <input type="text" placeholder="Name" required />
+                    </div>
+                    <div className="inputGroup">
+                        <input type="email" placeholder="Email" required />
+                    </div>
+                    <div className="inputGroup">
+                        <input type="password" placeholder="Password" required />
+                    </div>
+                    <div className="inputGroup">
+                        <input type="password" placeholder="Confirm Password" required />
+                    </div>
+                    <button className="registerButton">Register</button>
+                </form>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default RegistrationPopup
+export default RegistrationPopup;
